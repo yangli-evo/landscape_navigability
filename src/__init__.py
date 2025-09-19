edd87583653265c5e417dd0f7e58618f235aa177
Yang Li <yangli.evo@gmail.com>
2025-09-19 17:16:32 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,21 @@
-from .utils import classify_epistasis, classify_epistasis_P_N, get_P, get_cv
+__version__ = "0.1.0"
+
+# user could use from src import get_P
+from .utils import (
+    epsilon,
+    threshold_func,
+    classify_epistasis,
+    classify_epistasis_P_N,
+    get_P,
+    get_cv,
+)
+
+# control the public API
+__all__ = [
+    "epsilon",
+    "threshold_func",
+    "classify_epistasis",
+    "classify_epistasis_P_N",
+    "get_P",
+    "get_cv",
+]
